49b1bccad38b535af7d9a8ddb937fba4ec2018ea
kittisaker <kittisak.hanheam@gmail.com>
2023-10-18 21:17:55 +0700

    update chapter-12

--- TestComposition.java
+++
@@ -1,8 +0,0 @@
-public class TestComposition {
-    public static void main(String[] args) {
-        Processor intelProcessor = new Processor("Intel", "17");
-        Computer dellComputer = new Computer(intelProcessor, "Dell");
-
-        System.out.println(dellComputer);
-    }
-}
